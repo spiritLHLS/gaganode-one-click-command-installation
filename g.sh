5caf455d2f55f6e7234aa369eb4103f58ea14fb2
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-08 22:55:37 +0800

    Create g.sh

---
+++ g.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+#FROM https://github.com/spiritLHLS/gaganode-one-click-command-installation
+
+utf8_locale=$(locale -a 2>/dev/null | grep -i -m 1 -E "UTF-8|utf8")
+if [[ -z "$utf8_locale" ]]; then
+  echo "No UTF-8 locale found"
+else
+  export LC_ALL="$utf8_locale"
+  export LANG="$utf8_locale"
+  export LANGUAGE="$utf8_locale"
+  echo "Locale set to $utf8_locale"
+fi
+
+# 自定义字体彩色，read 函数，安装依赖函数
+red(){ echo -e "\033[31m\033[01m$1$2\033[0m"; }
+green(){ echo -e "\033[32m\033[01m$1$2\033[0m"; }
+yellow(){ echo -e "\033[33m\033[01m$1$2\033[0m"; }
+reading(){ read -rp "$(green "$1")" "$2"; }
+
+# 必须以root运行脚本
+check_root(){
+  [[ $(id -u) != 0 ]] && red " The script must be run as root, you can enter sudo -i and then download and run again." && exit 1
+}
+
+# 判断系统，并选择相应的指令集
+check_operating_system(){
+  CMD=("$(grep -i pretty_name /etc/os-release 2>/dev/null | cut -d \" -f2)"
+       "$(hostnamectl 2>/dev/null | grep -i system | cut -d : -f2)"
+       "$(lsb_release -sd 2>/dev/null)" "$(grep -i description /etc/lsb-release 2>/dev/null | cut -d \" -f2)"
+       "$(grep . /etc/redhat-release 2>/dev/null)"
+       "$(grep . /etc/issue 2>/dev/null | cut -d \\ -f1 | sed '/^[ ]*$/d')"
+      )
+
+  for i in "${CMD[@]}"; do SYS="$i" && [[ -n $SYS ]] && break; done
+
+  REGEX=("debian" "ubuntu" "centos|red hat|kernel|oracle linux|amazon linux|alma|rocky")
+  RELEASE=("Debian" "Ubuntu" "CentOS")
+  PACKAGE_UPDATE=("apt -y update" "apt -y update" "yum -y update")
+  PACKAGE_INSTALL=("apt -y install" "apt -y install" "yum -y install")
+  PACKAGE_UNINSTALL=("apt -y autoremove" "apt -y autoremove" "yum -y autoremove")
+
+  for ((int = 0; int < ${#REGEX[@]}; int++)); do
+    [[ $(echo "$SYS" | tr '[:upper:]' '[:lower:]') =~ ${REGEX[int]} ]] && SYSTEM="${RELEASE[int]}" && break
+  done
+
+  [[ -z $SYSTEM ]] && red " ERROR: The script supports Debian, Ubuntu, CentOS or Alpine systems only.\n" && exit 1
+}
+
+# 判断宿主机的 IPv4 或双栈情况
+check_ipv4(){
+  # 遍历本机可以使用的 IP API 服务商
+  API_NET=("ip.sb" "ipget.net" "ip.ping0.cc" "https://ip4.seeip.org" "https://api.my-ip.io/ip" "https://ipv4.icanhazip.com" "api.ipify.org" "ifconfig.co")
+
+  # 遍历每个 API 服务商，并检查它是否可用
+  for p in "${API_NET[@]}"; do
+    # 使用 curl 请求每个 API 服务商
+    response=$(curl -s4m8 "$p")
+    sleep 1
+    # 检查请求是否失败，或者回传内容中是否包含 error
+    if [ $? -eq 0 ] && ! echo "$response" | grep -q "error"; then
+      # 如果请求成功且不包含 error，则设置 IP_API 并退出循环
+      IP_API="$p"
+      break
+    fi
+  done
+
+  # 判断宿主机的 IPv4 、IPv6 和双栈情况
+  ! curl -s4m8 $IP_API | grep -q '\.' && red " ERROR：The host must have IPv4. " && exit 1
+}
+
+# 判断 CPU 架构
+check_virt(){
+  ARCHITECTURE=$(uname -m)
+  case "$ARCHITECTURE" in
+    aarch64 ) ARCH=arm64;;
+    x64|x86_64 ) ARCH=x86;;
+    * ) red " ERROR: Unsupported architecture: $ARCHITECTURE\n" && exit 1;;
+  esac
+}
+
+# 输入信息
+input_token(){
+  [ -z $token ] && reading " Enter your Token, if you do not find it, open https://dashboard.gaganode.com/register?referral_code=smowgcziqyrfhpo: " token
+}
+
+# 显示结果
+result(){
+  green " Finish \n"
+}
+
+# 卸载
+uninstall(){
+  
+}
+
+# 传参
+while getopts "UuM:m:" OPTNAME; do
+  case "$OPTNAME" in
+    'U'|'u' ) uninstall;;
+    'T'|'t' ) token=$OPTARG;;
+  esac
+done
+
+# 主程序
+check_root
+check_operating_system
+check_ipv4
+check_virt
+input_token
+if [ $SYSTEM = "CentOS" ]; then
+    yum update
+    yum install -y curl tar ca-certificates
+else
+    apt-get update
+    apt-get install -y curl tar ca-certificates
+fi
+if [ $ARCHITECTUREH = "amd64" ]; then
+    curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
+    ./apphub service remove && sudo ./apphub service install
+    ./apphub service start
+    sleep 3
+    ./apphub status
+    sleep 1
+    ./apps/gaganode/gaganode config set --token=${token}
+    sleep 1
+    ./apphub restart
+else
+    curl -o apphub-linux-arm64.tar.gz https://assets.coreservice.io/public/package/61/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-arm64.tar.gz && rm -f apphub-linux-arm64.tar.gz && cd ./apphub-linux-arm64
+    ./apphub service remove && sudo ./apphub service install
+    ./apphub service start
+    sleep 3
+    ./apphub status
+    sleep 1
+    ./apps/gaganode/gaganode config set --token=${token}
+    sleep 1
+    ./apphub restart
+fi
+result
