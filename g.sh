8aa8237d2cc00a719e578beb1f5f8e0793e81c6e
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2023-08-08 23:09:23 +0800

    Update g.sh

--- g.sh
+++ g.sh
@@ -111,7 +111,7 @@ else
     apt-get update
     apt-get install -y curl tar ca-certificates sudo 
 fi
-if [ $ARCHITECTUREH = "x86" ]; then
+if [[ $ARCHITECTURE == "x86" ]]; then
     curl -o apphub-linux-amd64.tar.gz https://assets.coreservice.io/public/package/60/app-market-gaga-pro/1.0.4/app-market-gaga-pro-1_0_4.tar.gz && tar -zxf apphub-linux-amd64.tar.gz && rm -f apphub-linux-amd64.tar.gz && cd ./apphub-linux-amd64
     sudo ./apphub service remove && sudo ./apphub service install
     sudo ./apphub service start
